f92a225742c52fc185792d6bbb03ea467923bcdc
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2024-09-11 20:57:27 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd /action
-mongodump --uri=$1
+mongodump --uri=$1 $2
 mkdir $GITHUB_WORKSPACE/dump
 mv dump $GITHUB_WORKSPACE/
 
 echo "Dump created and stored in $GITHUB_WORKSPACE/dump"
 
-if [ "$2" = "true" ]; then
+if [ "$3" = "true" ]; then
   tar -zcvf $GITHUB_WORKSPACE/dump.tar.gz $GITHUB_WORKSPACE/dump
   echo "Dump compressed and stored in $GITHUB_WORKSPACE/dump.tar.gz"
 else
