ee734b2333446dd0f3f4ece5a01cd7b24ed65ff9
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2024-09-11 19:43:51 +0300

    test

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,9 @@ rm -rf $GITHUB_WORKSPACE/dump
 
 if [ "$2" = "true" ]; then
   rm $GITHUB_WORKSPACE/dump.tar.gz
+  echo $(ls -la $GITHUB_WORKSPACE/dump.tar.gz)
+  echo "Compressed dump removed"
 fi
 
-echo $(ls -la $GITHUB_WORKSPACE)
+echo $(ls -la $GITHUB_WORKSPACE/dump)
 echo "Cleanup completed"
