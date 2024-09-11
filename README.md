350300362c5d07c88c459fee3ed47790915109f1
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2024-09-11 18:23:12 +0300

    BREAKING CHANGE: release

---
+++ README.md
@@ -0,0 +1,21 @@
+# Mongodump Action
+
+GitHub Action for creating a MongoDB dump using `mongodump`.
+
+## Example usage
+
+```yml
+name: Backup
+
+on:
+  schedule:
+    - cron: 0 0 * * *
+
+jobs:
+  backup:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: zahornyak/mongodump-action@v1
+        with:
+          connection-string: ${{ secrets.MONGO_URI }}
+```
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /action
+mongodump $1
+mkdir $GITHUB_WORKSPACE/dump
+cp -r dump $GITHUB_WORKSPACE/dump
+
+echo "Dump created and stored in $GITHUB_WORKSPACE/dump"
+
+if [ "$2" = "true" ]; then
+  tar -zcvf $GITHUB_WORKSPACE/dump.tar.gz $GITHUB_WORKSPACE/dump
+  echo "Dump compressed and stored in $GITHUB_WORKSPACE/dump.tar.gz"
+else
+  echo "Not compressing the dump"
+fi
+
