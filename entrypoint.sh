ee0f822de405afd6e0724443dbf115118109dacf
Andriy Zahornyak <andriyzzahornyak@gmail.com>
2024-09-12 10:59:56 +0300

    add possibility to change the mongo command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 cd /action
-mongodump --uri=$1 $2
-echo "full command: mongodump --uri=$1 $2"
+$1 --uri=$2 $3
+echo "full command: $1 --uri=$2 $3"
 mkdir $GITHUB_WORKSPACE/dump
 mv dump $GITHUB_WORKSPACE/
 
 echo "Dump created and stored in $GITHUB_WORKSPACE/dump"
 
-if [ "$3" = "true" ]; then
-  tar -zcvf $GITHUB_WORKSPACE/dump.tar.gz $GITHUB_WORKSPACE/dump
+if [ "$4" = "true" ] && [ "$1" = "mongodump" ]; then
+  cd $GITHUB_WORKSPACE
+  tar -zcvf dump.tar.gz dump
   echo "Dump compressed and stored in $GITHUB_WORKSPACE/dump.tar.gz"
 else
   echo "Not compressing the dump"
