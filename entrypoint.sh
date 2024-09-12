a036a1437089368e206aa368331aa3e5850ef517
Andriy Zahornyak <44653428+zahornyak@users.noreply.github.com>
2024-09-12 12:46:54 +0300

    fix: small fixes (#6)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /action
-$1 --uri=$2 $3
 echo "full command: $1 --uri=$2 $3"
-mv dump $GITHUB_WORKSPACE/
-if [ $? -eq 0 ]; then
+
+if [ "$1" = "mongodump" ] && $1 --uri=$2 $3
+then
   echo "Dump created and stored in $GITHUB_WORKSPACE/dump"
+else
+  echo "Not a mongodump command. Skipping moving dump"
 fi
 
 if [ "$4" = "true" ] && [ "$1" = "mongodump" ]; then
-  cd $GITHUB_WORKSPACE
   tar -zcvf dump.tar.gz dump
   echo "Dump compressed and stored in $GITHUB_WORKSPACE/dump.tar.gz"
 else
